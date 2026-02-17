55ec7f306a8c457ab49a81367c6c8be293809aea
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-17 17:50:59 +0200

    add the roll button to chage the value

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import {useState} from 'react'
 import './App.css'
 import Die from './components/Die'
-const dies = []
 function App() {
-      for(let i=0;i<5;i++){
-        const randomNumber = Math.ceil(Math.random()*6)
-        dies.push(randomNumber)
-      }
+
+
+  ///      Question : how i am looping five t((ime and get 10 number in array?
+  const[dies,setDies] = useState(generateDies())
+  function generateDies(){
+    const newDies = []
+    for(let i=0;i<10;i++){
+      newDies.push(Math.ceil(Math.random()*6))
+    }
+      return newDies
+  }
+
+
+
+   const diceElements = dies.map((die,index) => <Die key={index} value={die}/>)
+
+
+  function rollDies(){
+    setDies(generateDies())
+  }
   return (
+    
     <>
     <main>
      <div className="dice-container">
-        {dies.map((die,index)=><Die key={index} value={die}/>)}
+        {diceElements}
+      
      </div>
+     <button className="roll-dice" onClick={rollDies}>Roll Dice</button>
     </main>
     </>
   )
