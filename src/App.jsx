9981a836695c69abf17669db26fc783af13224a2
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-17 17:08:08 +0200

    add the die and getting the random value for each die

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './App.css'
+import Die from './components/Die'
+const dies = []
 function App() {
+      for(let i=0;i<5;i++){
+        const randomNumber = Math.ceil(Math.random()*6)
+        dies.push(randomNumber)
+      }
   return (
     <>
-    <h1>Tenzies Game</h1>
+    <main>
+     <div className="dice-container">
+        {dies.map((die,index)=><Die key={index} value={die}/>)}
+     </div>
+    </main>
     </>
   )
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,12 @@
+
+
+function Die(props) {
+    const{value}=props
+  return (
+    <>
+        <button>{value}</button>
+    </>
+  )
+}
+
+export default Die
