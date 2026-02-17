b42dfbeb366d7d3682b02f8a695a0b9b53b48bd4
Emmanuel Niyonsaba <emmanuelniyonsaba40@gmail.com>
2026-02-17 19:10:07 +0200

    rf: addding new component for strature

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css'
-import Die from './components/Die'
+import DiesDisplyer from './components/DiesDisplyer'
 function App() {
-
-
-  ///      Question : how i am looping five t((ime and get 10 number in array?
-  const[dies,setDies] = useState(generateDies())
-  function generateDies(){
-    const newDies = []
-    for(let i=0;i<10;i++){
-      newDies.push(Math.ceil(Math.random()*6))
-    }
-      return newDies
-  }
-
-
-
-   const diceElements = dies.map((die,index) => <Die key={index} value={die}/>)
-
-
-  function rollDies(){
-    setDies(generateDies())
-  }
+  
   return (
-    
     <>
-    <main>
-     <div className="dice-container">
-        {diceElements}
-      
-     </div>
-     <button className="roll-dice" onClick={rollDies}>Roll Dice</button>
-    </main>
+    <DiesDisplyer />
     </>
   )
 }
---
+++ src/components/DiesDisplyer.jsx
@@ -0,0 +1,30 @@
+import  { useState } from "react"
+import Die from "./Die"
+
+function DiesDisplyer() {
+    const [dies, setDies] = useState(generateDies())
+function generateDies() {
+    const newDies = []
+    for (let i = 0; i < 10; i++) {
+      newDies.push(Math.ceil(Math.random() * 6))
+    }
+    return newDies
+  }
+  const diceElements = dies.map((die, index) => <Die key={index} value={die} />)
+
+  function rollDies() {
+    setDies(generateDies())
+  }
+  return (
+   <>
+     <main>
+        <div className="dice-container">
+          {diceElements}
+        </div>
+        <button className="roll-dice" onClick={rollDies}>Roll Dice</button>
+      </main>
+   </>
+  )
+}
+
+export default DiesDisplyer
